2b6bfb1d63f62ae5bbe7ea3c0840b5cd1991652b
Dennis Idowu <dennis_idowu@live.com>
2020-02-19 13:07:26 -0500

    creating of user models for authentication process of users in database

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 //test:
 app.get("/", (req, res) => res.send("API Running"));
 
+//Defining Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
