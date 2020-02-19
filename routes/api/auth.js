68b08f3764e36c24e3d6c9263f5e069e473c8063
Dennis Idowu <dennis_idowu@live.com>
2020-02-19 18:15:39 -0500

    added logic to routes/api/auth to return user data upon verification of auth key via importing middleware/auth as well as models/User to check schema and user inputs are correct as well as produce data in schema format

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 //@router - test route
 //Get api/auth
 //access via pubic (private requires token)
-router.get("/", auth, (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    //returning user data w/id no password
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+    console.log(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
