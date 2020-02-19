0b30949be297c6b3ba9c0f4a53404ea3d40759fc
Dennis Idowu <dennis_idowu@live.com>
2020-02-19 13:45:11 -0500

    implemented express-validator to routes/users for the registration process/confirmation of fields being filled correctly prior to submission to database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //@route - POST request route
 //registration of user
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
