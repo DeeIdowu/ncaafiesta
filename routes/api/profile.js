e18028116bf4a84c6f28dfa4ba92679af2a1213c
Dennis Idowu <dennis_idowu@live.com>
2020-02-19 22:42:37 -0500

    added routes to get all profiles to get profiles by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -21,6 +23,106 @@ router.get("/me", auth, async (req, res) => {
     console.log(profile);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("location", "Location is required")
+        .not()
+        .isEmpty(),
+      check("bio", "Bio is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      team,
+      location,
+      bio,
+      youtube,
+      twitter,
+      facebook,
+      instagram
+    } = req.body;
+    //profile object:
+    const profileFields = {};
+    //fields added to  profile object:
+    profileFields.user = req.user.id;
+    if (team) profileFields.team = team;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    //for the social object of profile
+    profileFields.social = {};
+    if (youtube) profileFields.youtube = youtube;
+    if (twitter) profileFields.twitter = twitter;
+    if (facebook) profileFields.facebook = facebook;
+    if (instagram) profileFields.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({
+        user: req.user.id
+      });
+      if (profile) {
+        //update the fields if changed
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      //create new profile if no entry is found
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//@route  GET api/profile
+//@desc   To Get All Profiles
+//@access Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route  GET api/profile/user/:user_id
+//@desc   To Profile By User Id
+//@access Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile Not Found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile Not Found" });
+    }
     res.status(500).send("Server Error");
   }
 });
