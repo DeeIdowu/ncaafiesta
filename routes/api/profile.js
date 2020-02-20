6da6963ddaa79421ad9e474aaab4318bdc2e4736
Dennis Idowu <dennis_idowu@live.com>
2020-02-19 22:03:40 -0500

    created route to obtain user profile of current user that is logged in via get request and use of jwt token

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 //@router - test route
-//Get api/profile
+//Get api/profile/me to fetch data for user logged in
 //access via pubic (private requires token)
-router.get("/", (req, res) => res.send("Profile Route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name, avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+    console.log(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
